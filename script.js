743d8ba854c05eeb4775310d0ed8f590976ad6a2
shravani-kulkarni-13 <shravnik139@gmail.com>
2025-01-05 20:09:28 +0530

    authentication has been set

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const idpass = {
+    userName : "IndianPolice@100",
+    passWord : "policeforce100"
+};
 
+const policeLogin = (event) => {
+    event.preventDefault();
+
+    const userid = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const loginMessage = document.getElementById("loginMsg");
+
+    if(userid === idpass.userName && pass === idpass.passWord) {
+        loginMessage.style.color = "green";
+        loginMessage.textContent = "Logging in..."
+        setTimeout(() => {
+            window.location.href = "cases.html";
+        },3000);
+    }
+    else{
+        loginMessage.style.color = "red";
+        loginMessage.innerHTML = "Invalid username and password!!";
+        setTimeout(() => {
+            loginMessage.textContent = "";
+        }, 5000);
+    }
+}
